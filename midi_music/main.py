51a8605436bc3b1b42f16ee136c08a091dc29023
John Brittain <brittainjohn4@gmail.com>
2025-05-25 13:58:50 -0500

    creates mutliple tracks in one midi file

--- midi_music/main.py
+++ midi_music/main.py
@@ -6,29 +6,34 @@ from builder_midi import MidiTrackBuilder
 
 from utils import print_midi_message, print_midi_track, print_midi_file
 
+OUTPUT_FILENAME = 'example2.mid'
+
 def main():
     """Example usage of the midi-music-tool"""
 
-    MessageGenerator = MidiMessageGenerator()
-    TrackBuilder = MidiTrackBuilder()
+    message_generator = MidiMessageGenerator()
+    track_builder = MidiTrackBuilder()
 
     """Generate MIDI messages"""
+    tracks = []
     for i in range(5):
-        note_on = MessageGenerator.generate_note_on(channel=0, note=60+i, velocity=64, duration=480)
-        note_off = MessageGenerator.generate_note_off(channel=0, note=60+i, velocity=64)
-        midi_note = MessageGenerator.generate_midi_note(channel=0, note=60+i, velocity=64, duration=480)
-        cc = MessageGenerator.generate_control_change(channel=0, CCNumber=i, CCValue=i*10)
-        program = MessageGenerator.generate_program_change(channel=0, Program=i)
-        track = TrackBuilder.add(note_on).add(note_off).add(midi_note).add(cc).add(program).build()
+        note_on = message_generator.generate_note_on(channel=0, note=60+i, velocity=64, duration=480)
+        note_off = message_generator.generate_note_off(channel=0, note=60+i, velocity=64)
+        midi_note = message_generator.generate_midi_note(channel=0, note=60+i, velocity=64, duration=480)
+        cc = message_generator.generate_control_change(channel=0, CCNumber=i, CCValue=i*10)
+        program = message_generator.generate_program_change(channel=0, Program=i)
+        track = track_builder.add(note_on).add(note_off).add(midi_note).add(cc).add(program).build()
+        tracks.append(track)    
         
     # Create a MidiFileWriter instance
-    FileWriter = MidiFileWriter(filename='example2.mid')
-    # Add the track to the MidiFileWriter
-    FileWriter.add_track(track)
-    # Save the MIDI file
-    FileWriter.save()
+    file_writer = MidiFileWriter(filename=OUTPUT_FILENAME)
 
-    print_midi_file(FileWriter)
+    for track in tracks:
+        file_writer.add_track(track)
+
+    file_writer.save()
+
+    print_midi_file(file_writer)
 
 if __name__ == "__main__":
     main()
